3e9a87e2a5bccbcf4c6ab72f288a75c1cd28dfee
yaox12 <yaox12@outlook.com>
2017-08-28 16:43:08 +0800

    simulate a user login using Scrapy

---
+++ panda/panda/spiders/learn.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import json
+import six.moves
+import scrapy
+
+
+class PandaSpider(scrapy.Spider):
+    name = "learn"
+
+    def start_requests(self):
+        # use existing cookies
+        '''
+        urls = [
+            'http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/MyCourse.jsp?typepage=2',
+        ]
+        cookie = {'THNSV2COOKIE': 'MDY0MzczNjg3YmMyNTQ0MjgzODc3ODZjZjBlOTM2NDBgMjAxNjMxMDYyM2DSpvbOYFNgNTguMjAwLjEyOS4xMzBgMzc3MDk1NDYxYDE1MDM5MDUzNDk1NDRgMDFLNDdGTTRLRE43VVY4RkdKOUdSTUdOYA--',
+                  'JSESSIONID': 'abcIFjv0YwAzK53bVPN4v',
+        }
+        
+        for url in urls:
+            yield scrapy.Request(url, cookies=cookie, callback=self.parse)
+        '''
+        return [scrapy.FormRequest(url='https://learn.tsinghua.edu.cn/MultiLanguage/lesson/teacher/loginteacher.jsp',
+                                  formdata={'userid': 'yaox16', 'userpass': 'xy2011010044'},
+                                  callback=self.parse)]
+
+    def parse(self, response):  
+        cookie = response.headers.getlist('Set-Cookie')
+        # print(cookie)
+        return scrapy.Request(url='http://learn.tsinghua.edu.cn/MultiLanguage/lesson/student/MyCourse.jsp?typepage=2',
+                            #   cookies=cookie,
+                              callback=self.after_login)
+
+    def after_login(self, response):
+        with open('./learn_body.html', 'wb') as bodyfile:
+            bodyfile.write(response.body)
+            bodyfile.close()
+        
+        with open('learn_lessons.json', 'w') as jsonfile:
+            for lesson in response.css('tr[class*=info_tr]'):
+                json.dump({
+                    '课程名称': lesson.css('a::text').extract_first().strip(),
+                    'url': lesson.css('a::attr(href)').extract_first(),
+                }, jsonfile, indent=4, ensure_ascii=False)
+            jsonfile.close()
--- panda/panda/spiders/panda_spider.py
+++ panda/panda/spiders/panda_spider.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 from __future__ import absolute_import
@@ -24,14 +24,17 @@ class PandaSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
-        # with open('./body.html', 'wb') as outfile:
-        #     outfile.write(response.body)
-        with open('./panda.json', 'w') as outfile:
+        cookie = response.headers.getlist('Cookie')
+        print(cookie)
+        with open('./panda_body.html', 'wb') as bodyfile:
+            bodyfile.write(response.body)
+            bodyfile.close()
+        with open('./panda_videos.json', 'w') as jsonfile:
             for video in response.css('li.video-list-item'):
                 json.dump({
                     '直播链接': BASE_URL + video.css('a.video-list-item-wrap::attr(href)').extract_first(),
                     '直播标题': video.css('span.video-title::text').extract_first(),
                     '主播': ''.join(video.css('span[class*=nickname]::text').extract()).strip(),
                     '观看人数': video.css('span.video-number::text').extract_first(),
-                }, outfile, indent=4, ensure_ascii=False)
-            outfile.close()
+                }, jsonfile, indent=4, ensure_ascii=False)
+            jsonfile.close()
